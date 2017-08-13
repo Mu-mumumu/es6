67fbbabcc91a3740d157385f87bffbe3825c37df
fourWoods <224162391@qq.com>
2017-08-13 16:23:51 +0800

    let

---
+++ js/let.js
@@ -0,0 +1,64 @@
+//let 声明变量 只在命令所在的代码块有效
+
+
+{
+	let a=4
+	console.log("1a:"+a)
+	var b=2
+}
+console.log(b)
+//console.log(a)//报错
+
+//let 声明变量不会被提升
+
+console.log(c)
+var c=2   //undefined
+
+//console.log(d)
+//let d=2  //报错
+
+let f=2
+function kk(){
+	f=3
+	console.log(f)
+//	let f=4    //报错  如不加这句 则不报错   加了这句 22行为暂时性死区  即使已在全局声明过
+}
+kk()
+
+
+var g;
+var g;
+//let g  //报错  提示已声明过a
+
+
+let h;
+//let h;   //报错
+
+
+// let在for中的应用
+var wDiv=document.getElementsByTagName("div")
+
+//for (var i=0;i<wDiv.length;i++){
+//	wDiv[i].index=i*2;
+//	wDiv[i].onclick=function(){
+//		console.log(this.index)
+//	}
+//	
+//}
+
+//普通情况下 闭包
+//for (var i=0;i<wDiv.length;i++){
+//	(function(i){
+//		wDiv[i].onclick=function(){
+//			console.log(i)
+//		}
+//	})(i)
+//	
+//}
+//直接用let 
+for (let i=0;i<wDiv.length;i++){
+	wDiv[i].onclick=function(){
+		console.log(i)
+	}
+	
+}
